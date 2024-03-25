9bf68a117aa41f482eb05620833f66f7018a0346
andriwrs <andriw.r.superbi@gmail.com>
2024-03-25 04:17:08 -0300

    Primeiro commit

---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import React from "react"
+import styled from "styled-components"
+
+const Login = (props) => {
+    return ( 
+    <Container>
+        <Content>
+            <CTA>
+                <CTALogoOne src="/mages/cta-logo.one.png" alt="" />
+            </CTA>
+            <BgImage />
+        </Content> 
+    </Container>
+    );
+};
+
+const Container = styled.section `
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Content = styled.div `
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align_items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
+    color: #ffffff;
+`;
+
+const BgImage = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpg");
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+`;
+
+const CTA = styled.div`
+
+`;
+
+const CTALogoOne = styled.div`
+
+`;
+
+export default Login;
